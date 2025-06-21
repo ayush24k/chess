17821b5c076bc4bfade3faba86ca99358678e10f
ayush24k <ayushanshu69@gmail.com>
2025-06-21 14:36:31 +0530

    game init and handlemove and start game logic

--- server/src/GameManager.ts
+++ server/src/GameManager.ts
@@ -1,13 +1,16 @@
 import { WebSocket } from "ws";
-import { INIT_GAME } from "./messages";
+import { INIT_GAME, MOVE } from "./messages";
+import { Game } from "./Game";
 
 export class GameManager {
     private games: Game[];
-    private pendingUser: WebSocket;
+    private pendingUser: WebSocket | null;
     private users: WebSocket[];
 
-    constructor () {
-        this.games = []
+    constructor() {
+        this.games = [];
+        this.pendingUser = null;
+        this.users = [];
     }
 
     addUser(socket: WebSocket) {
@@ -17,24 +20,46 @@ export class GameManager {
 
     removeUser(socket: WebSocket) {
         this.users = this.users.filter((user) => {
-           return user != socket;
+            return user != socket;
         })
 
         // stop cuz user has left
     }
 
+    private startGame(socket: WebSocket) {
+        // check if there is a pending user
+        if (this.pendingUser) {
+            // start the game
+            const game = new Game(this.pendingUser, socket);
+            this.games.push(game);
+            this.pendingUser = null;
+        } else {
+            this.pendingUser = socket;
+        }
+    }
+
+    private handleMove(socket: WebSocket, message: string) {
+        // find the running game using player socket
+        const game = this.games.find((game) => {
+            return game.player1 === socket || game.player2 === socket
+        })
+
+        if (game) {
+            game.makeMove(socket, message)
+        }
+    }
+
     private addHandler(socket: WebSocket) {
         socket.on('message', (data) => {
             const message = JSON.parse(data.toString());
-            
+
             if (message === INIT_GAME) {
-                // check if there is a pending user
-                if (this.pendingUser) {
-                    // start the game
-                } else {
-                    this.pendingUser = socket;
-                }
-            } 
+                this.startGame(socket);
+            }
+
+            if (message === MOVE) {
+                this.handleMove(socket, message.move)
+            }
         })
     }
 
