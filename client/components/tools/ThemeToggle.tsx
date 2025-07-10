617536453a97847113236c546e4e3c959113cdc8
ayush24k <ayushanshu69@gmail.com>
2025-07-10 23:24:19 +0530

    themetoggle ui

--- client/components/tools/ThemeToggle.tsx
+++ client/components/tools/ThemeToggle.tsx
@@ -1,19 +1,51 @@
 'use client'
 import { useEffect, useState } from "react"
+import { motion } from 'motion/react';
+import { IconMoonFilled, IconSunFilled } from "@tabler/icons-react";
 
-export default function ThemeToggle () {
-    const [theme, setTheme] = useState('light');
+export default function ThemeToggle() {
+    const [theme, setTheme] = useState('dark');
 
     useEffect(() => {
         document.documentElement.classList.remove('light', 'dark');
         document.documentElement.classList.add(theme);
     }, [theme]);
 
+    function toggleTheme() {
+        setTheme(theme === 'light' ? 'dark' : 'light');
+    }
+
     return (
-        <button
-        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        >
-            {theme === 'light' ? <p>dark</p>: <p>light</p>}
-        </button>
+        <div>
+            <motion.button
+                onClick={toggleTheme}
+                className="p-2"
+                whileHover={{
+                    scale: 1.1
+                }}
+                whileTap={{
+                    scale: 0.8
+                }}
+                transition={{
+                    type: 'spring',
+                    stiffness: 300,
+                    damping: 15
+                }}
+            >
+                <motion.div
+                    initial={false}
+                    animate={{
+                        rotate: theme === 'dark' ? 0 : 180
+                    }}
+                    transition={{
+                        duration: 0.3
+                    }}
+                >
+                    {theme === 'dark' 
+                    ? <IconMoonFilled className="h-5 w-5"/>
+                    : <IconSunFilled className="h-5 w-5"/>}
+                </motion.div>
+            </motion.button>
+        </div>
     )
 }
