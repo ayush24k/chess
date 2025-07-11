09324e986ba2c6aa2caec44813316a8dae9a1024
ayush24k <ayushanshu69@gmail.com>
2025-07-11 15:25:25 +0530

    frontend: Navbar

---
+++ client/components/landingPageComponents/HeroSection.tsx
@@ -0,0 +1,11 @@
+
+export default function HeroSection() {
+    return (
+        <div className="relative flex items-center justify-center min-h-screen overflow-hidden dark:bg-neutral-900 bg-neutral-200">
+            hello
+            <div className="absolute inset-0 dark:block hidden -z-10 rounded-b-3xl overflow-hidden opacity-80">
+                
+            </div>
+        </div>
+    )
+}
---
+++ client/components/ui/Button.tsx
@@ -0,0 +1,29 @@
+'use client'
+import { motion } from 'motion/react';
+
+export default function Button({ children }: {
+    children: React.ReactNode,
+}) {
+    return (
+        <motion.div
+            whileHover={{
+                scale: 1.05,
+                boxShadow: "0px 15px 80px rgba(255,255,255,0.2)",
+            }}
+            whileTap={{
+                scale: 0.9
+            }}
+
+            transition={{
+                duration: 0.2,
+                ease: 'easeInOut'
+            }}
+
+            className="group relative bg-green-500 px-4 py-1 rounded-2xl items-center justify-center flex dark:text-neutral-900 text-md shadow-[0px_1px_4px_0px_rgba(255,255,255,0.1)_inset,0px_-1px_2px_0px_rgba(255,255,255,0.1)_inset]">
+            <button className="">
+                {children}
+            </button>
+            <span className='absolute inset-x-0 bottom-0 bg-gradient-to-r from from-transparent via-green-700 to-transparent h-[2px] w-3/5 mx-auto blur-sm'></span>
+        </motion.div>
+    )
+}
